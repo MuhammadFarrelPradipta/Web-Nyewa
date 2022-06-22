9c47ee3c6d414182592f6badcbe5e4b9988b5660
Muhammad Farrel Pradipta <79037809+MuhammadFarrelPradipta@users.noreply.github.com>
2022-06-22 18:47:12 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "u2003040123";
-$password = "u2003040123";
-$db = "dbu2003040123";
+$username = "root";
+$password = "";
+$db = "sewa";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
